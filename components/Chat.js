8f5a0b188724fc0bee0fc559cdc8d0c62aebd317
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-06-29 16:54:23 -0400

    Rebuilding Chat.js with React Native Gifted Chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,32 +1,50 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, View } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
+
 
 const Chat = ({ route, navigation }) => {
-    const { name, background } = route.params;
+    const { name } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
 
     useEffect(() => {
-        navigation.setOptions({ title: name });
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+        ]);
+    }, []);
+
+
+    useEffect(() => {
+        navigation.setOptions({ title: name })
     }, []);
 
     return (
-        <View style={{ ...styles.container, backgroundColor: background }}>
-            <Text style={styles.text}>Welcome, {name}! Chat coming soon!</Text>
-        </View>
-    );
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: 1
+            }}
+        />
+    )
+
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    text:
-    {
-        fontSize: 16,
-        fontWeight: "300",
-        color: "#757083"
+        flex: 1
     }
 });
 
